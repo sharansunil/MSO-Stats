c8c51589d64add9b908007c44b2f4c3a57987b82
sharansunil <sharansnl@gmail.com>
2019-04-15 15:55:37 +0800

    Centered Regression, One and Two way ANOVA done

--- experiments.R
+++ experiments.R
@@ -9,7 +9,7 @@ library(magrittr)
 
 #initial data load
 df<-read_excel('data.xlsx')
-df$month<-format(as.Date(df$order_date), "%y%m")
+df$month<-as.numeric(format(as.Date(df$order_date), "%y%m"))
 df$day<-strftime(df$order_date,"%A")
 df<-df %>% select(order_date,country,orders,utr,month,day)
 is_wknd<-function(x){
@@ -21,85 +21,39 @@ is_wknd<-function(x){
 }
 df$is_wknd<-as.factor(sapply(df$day,is_wknd))
 #initializee dummies
-
-df_reg<-df %>% group_by(month,country) %>% summarise(mean(orders))
-
-for(level in unique(df_reg$country)){
-  if(level=="Singapore"){
+months<-unique(df$month)
+rvec<-c()
+for(d in df$month){
+  for(i in seq(1,13)){
+    if(d==months[i]){
+      rvec<-c(rvec,i)
     }
-  else if(level=="Hong Kong"){
-    df_reg[paste("dummy", "HK", sep = "_")] <- ifelse(df_reg$country == level, 1, 0)
-  }
-  else{
-  df_reg[paste("dummy", level, sep = "_")] <- ifelse(df_reg$country == level, 1, 0)
-  }
+  } 
 }
-
-#prepare df
-df_growths<-df_reg[FALSE,] %>% select(month,country,`mean(orders)`)
-colnames(df_growths) = c("month","country","mean_order_growth")
-
-#generate monthly growth rates
-for(c in unique(df_reg$country)){
-  d=df_reg %>%  filter(country==c)
-  growths<-function(d){
-    x=d$`mean(orders)`
-    i=1
-    revec=c()
-    print(length(x))
-    while(i<length(x)){
-      revec[i]=(x[i+1]-x[i])*100/x[i]
-      i=i+1
-    }
-    fvec=cbind(d$month,d$country,c(0,revec))
-    colnames(fvec)=c("month","country","mean_order_growth")
-    return(fvec)
-  }
-  dx_=tbl_df(growths(d))
-  dx_$mean_order_growth %<>% as.double
-  print(dx_)
-  df_growths<-bind_rows(df_growths,dx_)
-}
-
-rm(list=c("dx_","d","c","level"))
-
-df_reg<-left_join(df_reg,df_growths,by=c("month","country"))
-df_reg<- df_reg %>%  select(month,country,mean_order_growth,dummy_Bangladesh,dummy_HK,dummy_Malaysia,dummy_Pakistan,dummy_Philippines,dummy_Taiwan,dummy_Thailand)
-df_reg<-df_reg %>% filter(month!="1804") #removes first month as no growth
-df_reg$month <- as.numeric(df_reg$month) #converts month to numeric for regression
-df_reg$country <- as.factor(df_reg$country) #converts country to factor
+df$index<-rvec 
+df$country<-as.factor(df$country)
 
 
-#MoM order growth: month x country
-
-encoded.growth.lm<-lm(mean_order_growth~month+dummy_Bangladesh+dummy_Thailand+dummy_Pakistan+dummy_HK+dummy_Malaysia+dummy_Philippines+dummy_Taiwan + dummy_Bangladesh*month+dummy_Thailand*month+dummy_Pakistan*month+dummy_HK*month+dummy_Malaysia*month+dummy_Philippines*month+dummy_Taiwan*month,data=df_reg)
-
-growth.lm<-lm(mean_order_growth~month+country+country*month,data=df_reg)
-
-
-summary(encoded.growth.lm)
-summary(growth.lm)
-AIC(encoded.growth.lm)
-AIC(growth.lm)
-plot(encoded.growth.lm)
-plot(growth.lm)
-
-#average daily orders: weekend x country x month
-df_wk<- df
-df_wk <- df_wk %>% group_by(country,month,is_wknd) %>% summarise(mean(orders))
-colnames(df_wk) <- c("country","month","is_wknd","mean_orders")
-df_wk$month <-as.numeric(df_wk$month)
-
-wkend.lm<-lm(mean_orders~month+country+is_wknd+country*is_wknd+country*month+is_wknd*month+month*is_wknd*country,data=df_wk)
-
-summary(wkend.lm)
-AIC(wkend.lm)
-plot(wkend.lm)
-
-
+#country vs time regression
+df.country.lm<-df %>% select(index,country,orders)
+df.country.lm$index<-scale(df.country.lm$index)
+country.lm<-lm(orders~country*index,data=df.country.lm)
+summary(country.lm)
+ggplot(data=df.country.lm,aes(x=index,y=orders)) + geom_smooth(method='lm')
 #interaction plots
 df.int<-df %>% group_by(country,is_wknd) %>% summarise(mean(orders))
 colnames(df.int)<-c("country","is_wknd","mean_orders")
+predicted_df <- data.frame(order_pred = predict(country.lm, df.country.lm), index=scale(df$index))
 
-country.weekend.int<-ggplot(df.int,aes(x=is_wknd,y=mean_orders,colour=country))+geom_line(aes(group=country))+geom_point()
+ggplot(df.country.lm,aes(x=index,y=orders,colour=country))+ geom_point() + geom_line(color='red',data = predicted_df, aes(x=index, y=order_pred)) 
+
+
+#boxplot overall
+ggplot(data=df,aes(x=month,y=orders,colour=country))+geom_boxplot()+facet_grid(~country)
+
+aov(data=df.int,mean_orders~country*is_wknd)
+qqplot(x=df$order_date,y=df$orders)
+summary(aov(mean_orders~country+is_wknd+country:is_wknd,data=df.int))
+
+
 
