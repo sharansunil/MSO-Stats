c14862ef3ce9d76d9ccb2b2e0f30ec185bc6777a
sharansunil <sharansnl@gmail.com>
2019-04-16 22:20:49 +0800

    update

--- experiments.R
+++ experiments.R
@@ -2,14 +2,12 @@ rm(list=ls())
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
-library(readr)
 library(readxl)
-library(GGally)
 library(magrittr)
+library(car)
 
-#DATA LOADING + CLEANING
-df<-read_excel('data.xlsx')
-head(df)
+
+##HELPER FUNCTIONS
 frame_prep<-function(df){
   
 
@@ -38,55 +36,64 @@ df$time<-rvec
 df$country<-as.factor(df$country)
 return(df)
 }
-df<-frame_prep(df)
+center_this <- function(x){
+  (x - mean(x, na.rm=TRUE))
+}
 
+#DATA LOADING + CLEANING
+df<-read_excel('data.xlsx')
+head(df)
+df<-frame_prep(df)
 
 #DATASET EXPLORATION
 qqplot(x=df$order_date,y=df$orders) #heavy tailed normal distribution
-
 ggplot(data=df,aes(x=country,y=orders,colour=country))+geom_boxplot()#boxplots to show data spread
 
 ###########REGRESSION: COUNTRY X TIME###########
 
 #DF PREP
-df.country<-df %>% select(time,country,orders,order_date)
-df.country$time<-scale(df.country$time)
-df.country$orders<-scale(scale=FALSE,df.country$orders)
-df.country$order_date<-scale(df.country$order_date)
+df_lm<-df %>% select(order_date,country,orders,order_date) #select fields
+df_lm<-df_lm %>% mutate(orders =center_this(orders)) #center orders
+df_lm$order_date<-scale(df_lm$order_date) #scale order dates
 
-#PAIRWISE COR, KRUSKAL WALLIS
-cor(df.country$time,df.country$orders) #shows little pairwose correlation between month and orders
-kruskal.test(orders~order_date,data=df.country) #shows little pairwise correl between date and orders
-kruskal.test(orders~country,data=df.country) #shows little pairwise correl between country and orders
-pairwise.wilcox.test(df.country$orders,df.country$country,p.adjust.method = "BH") #pairwise wilcox to see if dependencies within countries
+#TRAIN/TEST SPLIT
+set.seed(101) # Set Seed so that same sample can be reproduced in future also
+# Now Selecting 75% of data as sample from total 'n' rows of the data  
+sample <- sample.int(n = nrow(df_lm), size = floor(.9*nrow(df_lm)), replace = F)
+df_lm_ <- df_lm[sample, ]
+df_test  <- df_lm[-sample, ]
+df_lm<-df_lm_
 
-#REGRESSION EVAL
-country.lm<-lm(orders~country*scale(order_date),data=df.country)
+#PAIRWISE CORRELATIONS
+cor(df_lm$orders,df_lm$order_date) #shows little pairwise correl between day and orders
+pairwise.t.test(df_lm$orders, df_lm$country,p.adjust.method = "BH", pool.sd = FALSE)#heavy tailed normal so t test
+#day and country obviously unrelated
+
+#REGRESSION WITH INTERACTION ON TRAINING SET
+country.lm<-lm(orders~country*order_date,data=df_lm)
 summary(country.lm)
+plot(country.lm) #plot 1 check for homoscedacity. plot 2 qqplot plot 3 smth. plot 4 als homoscedastic.
 car::vif(country.lm) ###results show no multicollinearity
+AIC(country.lm)
+
+
+#REGRESSION WITHOUT INTERACTION ON TRAINING SET
+basiclm<-lm(orders~country+order_date,data=df_lm)
+summary(basiclm)
+car::vif(basiclm) ###results show no multicollinearity
+AIC(basiclm)
+plot(basiclm)
+
+#PREDICTION OF MODEL ON TEST SET
+set.seed(123)
+predicted_orders_country<-predict(country.lm,df_test)
+predicted_orders_basic<-predict(basiclm,df_test)
+test_eval<-cbind(df_test,predicted_orders_country,predicted_orders_basic) %>% sample_n(100)
+ggplot(test_eval,aes(x=order_date))+geom_line(aes(y=orders,colour="orders"))+geom_line(aes(y=predicted_orders_country,colour="predicted_orders_country"))+geom_line(aes(y=predicted_orders_basic,colour="predicted_orders_basic"))
+test_eval$res<-test_eval$orders-test_eval$predicted_orders
 
 #REGRESSION VIZ
-ggplot(data=df.country.lm,aes(x=index,y=orders)) + geom_smooth(method='lm')
-predicted_df <- data.frame(order_pred = predict(country.lm, df.country.lm), index=scale(df$index))
-ggplot(df.country.lm,aes(x=index,y=orders,colour=country))+ geom_point() + geom_line(alpha=0.4,size=2,color='red',data = predicted_df, aes(x=index, y=order_pred)) 
-
-
-#############ANOVA: WEEKEND X COUNTRY EFFECTS###################
-
-#interaction plots country vs weekend
-df.int<-df %>% group_by(country,is_wknd) %>% summarise(mean(orders))
-colnames(df.int)<-c("country","is_wknd","mean_orders")
-df.int$centered_mean_orders<-scale(scale=FALSE,df.int$mean_orders)
-ggplot(data=df.int,aes(x=is_wknd,y=centered_mean_orders,colour=country,group=country))+geom_point()+geom_line()
-
-#ONE WAY ANOVA FOR EACH COUNTRY X WKND
-summary(aov(orders~is_wknd,data=df %>% filter(country=="Malaysia")))
-summary(aov(orders~is_wknd,data=df %>% filter(country=="Singapore")))
-summary(aov(orders~is_wknd,data=df %>% filter(country=="Taiwan")))
-summary(aov(orders~is_wknd,data=df %>% filter(country=="Philippines")))
-summary(aov(orders~is_wknd,data=df %>% filter(country=="Hong Kong")))
-summary(aov(orders~is_wknd,data=df %>% filter(country=="Pakistan")))
-summary(aov(orders~is_wknd,data=df %>% filter(country=="Bangladesh")))
-summary(aov(orders~is_wknd,data=df %>% filter(country=="Thailand")))
-summary(aov(orders~is_wknd*country,data=df))
+ggplot(data=df_lm,aes(x=order_date,y=orders)) + geom_smooth(method='lm')
+predicted_df <- data.frame(order_pred = predict(country.lm, df_lm), order_date=df_lm$order_date)
+ggplot(df_lm,aes(x=order_date,y=orders,colour=country))+ geom_point() + geom_line(alpha=0.4,size=1,color='black',data = predicted_df, aes(x=order_date, y=order_pred)) 
 
